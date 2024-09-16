9959d5220800a415837213f81c16465045162eaf
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2024-09-15 22:47:49 -0400

    Lab 3 draft

--- lab/data/_inflation-data-prep.R
+++ lab/data/_inflation-data-prep.R
@@ -1,31 +1,27 @@
 # load packages ----------------------------------------------------------------
 
 library(tidyverse)
+library(readxl)
 
 # data prep for country inflation / pivot --------------------------------------
 
-# data source: https://stats.oecd.org
+# data source: https://data-explorer.oecd.org/vis?df[ds]=DisseminateFinalDMZ&df[id]=DSD_KEI%40DF_KEI&df[ag]=OECD.SDD.STES&dq=GBR%2BUSA%2BTUR%2BCHE%2BSWE%2BESP%2BSVN%2BSVK%2BPRT%2BPOL%2BNOR%2BNZL%2BNLD%2BMEX%2BLUX%2BLTU%2BLVA%2BKOR%2BJPN%2BITA%2BISR%2BIRL%2BISL%2BHUN%2BGRC%2BDEU%2BFIN%2BCRI%2BEST%2BDNK%2BCZE%2BFRA%2BCOL%2BCHL%2BCAN%2BBEL%2BAUT%2BAUS.A.CP.GR...G1&pd=1993%2C2023&to[TIME_PERIOD]=false&vw=tb
 
-country_inflation_long <- read_csv("labs/data/KEI_02022024035830117.csv.csv") |>
-  select(Country, Time, Value) |>
-  rename(
-    country = Country,
-    year = Time,
-    annual_inflation = Value
-    )
+country_inflation_raw <- read_excel("lab/data/OECD.SDD.STES,DSD_KEI@DF_KEI,,filtered,2024-09-15 21-01-38.xlsx", skip = 4)
 
-country_inflation_wide <- country_inflation_long |>
-  pivot_wider(
-    id_cols = country,
-    names_from = year,
-    values_from = annual_inflation
-  )
+country_inflation_wide <- country_inflation_raw |>
+  slice(-1) |>
+  slice(-39, -40) |>
+  select(-`Time period...2`, -`...34`) |>
+  rename(country = `Time period...1`)
 
-write_csv(country_inflation_wide, "labs/data/country-inflation.csv")
+write_csv(country_inflation_wide, "lab/data/country-inflation.csv")
 
 # data prep for us inflation / join --------------------------------------------
 
-us_inflation_raw <- read_csv("labs/data/PRICES_CPI_19092022031404285.csv")
+# data source: 
+
+us_inflation_raw <- read_csv("lab/data/PRICES_CPI_19092022031404285.csv")
 
 us_inflation_temp <- us_inflation_raw |>
   filter(Measure == "Percentage change on the same period of the previous year") |>
@@ -60,11 +56,11 @@ cpi_divisions <- us_inflation_temp |>
     description = cpi_division_description
   )
 
-write_csv(us_inflation, "labs/data/us-inflation.csv")
-write_csv(cpi_divisions, "labs/data/cpi-divisions.csv")
+write_csv(us_inflation, "lab/data/us-inflation.csv")
+write_csv(cpi_divisions, "lab/data/cpi-divisions.csv")
 
 # join: join other inflation indices for the US --------------------------------
 
-us_inflation <- read_csv("labs/data/us-inflation.csv")
-cpi_divisions <- read_csv("labs/data/cpi-divisions.csv")
+us_inflation <- read_csv("lab/data/us-inflation.csv")
+cpi_divisions <- read_csv("lab/data/cpi-divisions.csv")
 
